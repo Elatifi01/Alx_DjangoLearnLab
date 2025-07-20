7e9fb6ccbc3a622af0ce175be649b3ce6b92e64e
Haitam El Atifi <elatifi.haitam01@gmail.com>
2025-07-20 18:15:28 +0100

    Add views and URL routing with templates for books and libraries

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.detail import DetailView
+from .models import Book, Library
+
+# Function-Based View
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+# Class-Based View
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
